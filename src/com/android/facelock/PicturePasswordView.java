23956d550279b0b8eae0f769e0dcf27d307a942e
Gran PC <gran.pc@gmail.com>
2014-05-10 13:28:37 +0200

    Remove unneeded import

--- src/com/android/facelock/PicturePasswordView.java
+++ src/com/android/facelock/PicturePasswordView.java
@@ -12,7 +12,6 @@ import android.graphics.PointF;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.widget.ImageView;
