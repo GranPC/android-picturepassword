e2c3935caba247a8bb4c15b1fae0851d76ba74ca
Gran PC <gran.pc@gmail.com>
2014-05-11 16:23:43 +0200

    Xposed workaround for yet another (ROM-specific?) keyguard bug

--- src/com/android/facelock/XposedHelper.java
+++ src/com/android/facelock/XposedHelper.java
@@ -3,22 +3,24 @@ package com.android.facelock;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 import android.content.res.Resources;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedHelper implements IXposedHookLoadPackage
 {
-
 	public void handleLoadPackage( final LoadPackageParam lpparam ) throws Throwable
 	{
 		try
 		{
 			if ( !lpparam.packageName.equals( "com.android.keyguard" ) ) return;
 			
+			// Disable cancel button
 			findAndHookMethod( "com.android.keyguard.FaceUnlock", lpparam.classLoader, "initializeView", View.class, new XC_MethodHook()
 			{
 				@Override
@@ -28,6 +30,16 @@ public class XposedHelper implements IXposedHookLoadPackage
 					Resources res = v.getResources();
 					ImageButton cancel = ( ImageButton ) v.findViewById( res.getIdentifier( "face_unlock_cancel_button", "id", "com.android.keyguard" ) );
 					cancel.setVisibility( View.GONE );
+				}
+			} );
+			
+			// Fix padlock button being impossible to press (??)
+			findAndHookMethod( "com.android.keyguard.SlidingChallengeLayout", lpparam.classLoader, "onMeasure", int.class, int.class, new XC_MethodHook()
+			{
+				@Override
+				protected void afterHookedMethod( MethodHookParam param ) throws Throwable
+				{
+					XposedHelpers.callMethod( param.thisObject, "setChallengeInteractive", true );
 				}
 			} );
 		}
